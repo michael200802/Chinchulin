f038ef0b5fc37660955a6358411c8e944a199f9e
michael200802 <reyesespinozamichael@gmail.com>
2023-04-18 07:20:08 -0500

    entity fixed: mpfr_t members initializated

--- planet_sys/plt_sys_entity.c
+++ planet_sys/plt_sys_entity.c
@@ -1,20 +1,32 @@
 #include "plt_sys_entity.h"
 
+void __plt_sys_entity_init(plt_sys_entity_t * ent)
+{
+	mpfr_inits2(MPFR_DEFAULT_PRECISION,
+		ent->coord.cur_angle,ent->coord.inclination_angle,
+		ent->coord.X,ent->coord.Y,
+		ent->body.radius,ent->body.mass,
+		ent->trajectory.aphelion,ent->trajectory.perihelion,ent->trajectory.big_mass,
+		(mpfr_ptr)0);
+}
+
 void plt_sys_entity_init(
         plt_sys_entity_t * ent,
         mpfr_t radius, mpfr_t mass,//plt_sys_body_attr_t
-        entity_direction_t direction, mpfr_t aphelion, mpfr_t perihelion, void (*update_coord)(void*, mpfr_t),//plt_sys_trajectory_t
+        entity_direction_t direction, mpfr_t aphelion, mpfr_t perihelion, mpfr_t big_mass, void (*update_coord)(void*, mpfr_t),//plt_sys_trajectory_t
         mpfr_t inclination_angle, mpfr_t phase_angle, plt_sys_coord_t * origin//plt_sys_coord_t
 )
 {
+	__plt_sys_entity_init(ent);
+
 	mpfr_set(ent->body.radius,radius,MPFR_RNDN);
 	mpfr_set(ent->body.mass,mass,MPFR_RNDN);
 
 	ent->trajectory.direction = direction;
 	mpfr_set(ent->trajectory.aphelion,aphelion,MPFR_RNDN);
 	mpfr_set(ent->trajectory.perihelion,perihelion,MPFR_RNDN);
+	mpfr_set(ent->trajectory.big_mass,big_mass,MPFR_RNDN);
 	ent->trajectory.update_coord = update_coord;
-
 
 	mpfr_set(ent->coord.inclination_angle,inclination_angle,MPFR_RNDN);
 	mpfr_set(ent->coord.cur_angle,phase_angle,MPFR_RNDN);
@@ -24,6 +36,43 @@ void plt_sys_entity_init(
 	MPFR_DECL_INIT(stime,DEFAULT_PREC);
 	mpfr_set_ui(stime,0,MPFR_RNDN);
 	update_coord(ent,stime);
+
+}
+
+void plt_sys_get_real_coord(
+		mpfr_t x_val, mpfr_t y_val,
+                plt_sys_coord_t * coord
+        )
+{
+	MPFR_DECL_INIT(module,MPFR_DEFAULT_PRECISION);
+	MPFR_DECL_INIT(new_angle,MPFR_DEFAULT_PRECISION);
+	MPFR_DECL_INIT(x_val_aux,MPFR_DEFAULT_PRECISION);
+	MPFR_DECL_INIT(y_val_aux,MPFR_DEFAULT_PRECISION);
+
+	mpfr_set_ui(x_val,0,MPFR_RNDN);
+	mpfr_set_ui(y_val,0,MPFR_RNDN);
+
+	for(int i = 0;coord;i++)
+	{
+		mpfr_pow_ui(x_val_aux,coord->X,2,MPFR_RNDN);
+		mpfr_pow_ui(y_val_aux,coord->Y,2,MPFR_RNDN);
+		mpfr_add(module,x_val_aux,y_val_aux,MPFR_RNDN);
+		mpfr_sqrt(module,module,MPFR_RNDN);
+
+		mpfr_add(new_angle,coord->cur_angle,coord->inclination_angle,MPFR_RNDN);
+
+		mpfr_cos(x_val_aux,new_angle,MPFR_RNDN);
+		mpfr_sin(y_val_aux,new_angle,MPFR_RNDN);
+
+		mpfr_mul(x_val_aux,x_val_aux,module,MPFR_RNDN);
+		mpfr_mul(y_val_aux,y_val_aux,module,MPFR_RNDN);
+
+		mpfr_add(x_val,x_val,x_val_aux,MPFR_RNDN);
+		mpfr_add(y_val,y_val,y_val_aux,MPFR_RNDN);
+
+                coord = coord->origin;
+
+	}
 }
 
 void plt_sys_entity_nomove_init(
@@ -32,11 +81,16 @@ void plt_sys_entity_nomove_init(
                 mpfr_t x_pos, mpfr_t y_pos
         )
 {
+	__plt_sys_entity_init(ent);
+
         mpfr_set(ent->body.radius,radius,MPFR_RNDN);
         mpfr_set(ent->body.mass,mass,MPFR_RNDN);
 
 	ent->trajectory.direction = ENTDIR_NOMOVE;
+	ent->trajectory.update_coord = update_coord_nomove;
 
+	mpfr_set_ui(ent->coord.inclination_angle,0,MPFR_RNDN);
+	mpfr_set_ui(ent->coord.cur_angle,0,MPFR_RNDN);
 	mpfr_set(ent->coord.X,x_pos,MPFR_RNDN);
 	mpfr_set(ent->coord.Y,y_pos,MPFR_RNDN);
 	ent->coord.origin = NULL;
@@ -52,6 +106,7 @@ void plt_sys_entity_destroy(
 
 	mpfr_clear(ent->trajectory.aphelion);
 	mpfr_clear(ent->trajectory.perihelion);
+	mpfr_clear(ent->trajectory.big_mass);
 
 	mpfr_clear(ent->coord.X);
 	mpfr_clear(ent->coord.Y);
--- planet_sys/plt_sys_entity.h
+++ planet_sys/plt_sys_entity.h
@@ -1,13 +1,17 @@
 #pragma once
 
+#include <stdio.h>
+
 #include "types.h"
 
 #include "update_coord.h"
 
+#define MPFR_DEFAULT_PRECISION 200
+
 void plt_sys_entity_init(
 		plt_sys_entity_t * ent,
 		mpfr_t radius, mpfr_t mass,//plt_sys_body_attr_t
-		entity_direction_t direction, mpfr_t aphelion, mpfr_t perihelion, void (*update_coord)(void*,mpfr_t),//plt_sys_trajectory_t
+		entity_direction_t direction, mpfr_t aphelion, mpfr_t perihelion, mpfr_t big_mass, void (*update_coord)(void*,mpfr_t),//plt_sys_trajectory_t
 		mpfr_t inclination_angle, mpfr_t phase_angle, plt_sys_coord_t * origin//plt_sys_coord_t
 	);
 void plt_sys_entity_nomove_init(
@@ -15,6 +19,10 @@ void plt_sys_entity_nomove_init(
                 mpfr_t radius, mpfr_t mass,//plt_sys_body_attr_t
 		mpfr_t x_pos, mpfr_t y_pos
         );
+void plt_sys_get_real_coord(
+                mpfr_t x_val, mpfr_t y_val,
+                plt_sys_coord_t * coord
+	);
 void plt_sys_entity_destroy(
 		plt_sys_entity_t * ent
 	);
